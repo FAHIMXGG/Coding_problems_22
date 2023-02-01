d3f24f6f146ad90d68179cf22703053c6130fed8
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-02-01 23:57:59 +0600

    mid

--- search.js
+++ search.js
@@ -1,3 +1,23 @@
 const text = 'akash vora tara hoga mara shara'
 const doesExist = text.includes('tara');
 console.log(doesExist);
+
+// for case
+const text2 = "Blackpink is a South Korean girl group formed by YG Entertainment,      consisting of members Jisoo, Jennie, Rosé, and Lisa.";
+const searchString = 'JENNIE';
+const text2Lower = text2.toLocaleLowerCase();
+const doesExist2 = text2Lower.includes(searchString.toLocaleLowerCase());
+console.log(doesExist2);
+
+//index
+//console.log(text2,indexOf('girl'));
+if(text2.indexOf('Jisoo') != -1){
+    console.log('ok')
+}
+else{
+    console.log('none')
+}
+//start with
+console.log(text2.startsWith('Blackpink'))
+//end
+console.log(text2.endsWith('Lisa.'))
---
+++ split-join.js
@@ -0,0 +1,15 @@
+const text2 = "Blackpink is a South Korean girl group formed by YG Entertainment,      consisting of members Jisoo, Jennie, Rosé, and Lisa.";
+const part = text2.split(' ');
+const sentences = text2.split(',');
+const chara = text2.split('');
+//console.log(chara);
+//console.log(sentences);
+//console.log(part);
+
+//sclice
+const sli = text2.slice(5, 8);
+console.log(sli);
+
+//sub
+const sli2 = text2.substring(5, 8);
+console.log(sli2);
