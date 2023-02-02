e5927aae48691223c7aa496fa3fb70fd32de3253
FAHIMXGG <ahasanulhaquefahim1@gmail.com>
2023-02-02 22:04:41 +0600

    done

--- fibo.js
+++ fibo.js
@@ -1,5 +1,5 @@
 const fibo = [0, 1];
-for (let i = 2; i < 100; i++) {
+for (let i = 2; i <= 100; i++) {
     fibo[i]= fibo[i-1] + fibo[i-2];
     
 }
---
+++ pr.js
@@ -0,0 +1,7 @@
+const result2 = Math.ceil(2.0001);
+console.log(result2);
+
+function add(a, b){
+    return a + b;
+  }
+  console.log(add("adam" + "eve"))
